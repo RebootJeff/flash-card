a70d9d126b2261a867ed648782ed10807f1e96cc
Jeffrey Lee <jlee@zignallabs.com>
2015-10-28 20:42:44 -0700

    Begin work on data crawling

--- scripts/data.js
+++ scripts/data.js
@@ -1,3 +1,49 @@
+'use strict';
+
+var R = require('ramda');
+var request = require('request-promise');
+var cheerio = require('cheerio');
+
+function mineZignalTeamPage() {
+  return request('http://zignallabs.com/company/#our-team')
+    .then(extractProfilesFromHtml);
+}
+
+function extractProfilesFromHtml(htmlString) {
+  var $ = cheerio.load(htmlString);
+  var $profiles = $('.element-team');
+  var jsonProfiles = $profiles.map(function(index, profileEl) {
+    var $profile = $(profileEl);
+    // extract the following data:
+    // - employee category (get value for root element attr `data-project-cat`)
+    // - img src (CSS selector: attachment-portfolio-thumb)
+    // - employee name (CSS selector: h3 tag)
+    // - employee title (CSS selector: b tag)
+    // - employee blurb (not sure how to select, but comes after a br tag)
+    return {
+      category: $profile.attr('data-project-cat'),
+      photoUrl: $profile.find('.attachment-portfolio-thumb').attr('src'),
+      name: $profile.find('h3').text(),
+      title: $profile.find('b').text(),
+      blurb: getProfileBlurb($profile)
+    };
+  });
+
+  // Cheerio's `map` returns a Cheerio collection, so we need to convert to array
+  return convertObjToArray(jsonProfiles);
+}
+
+// Going after $profile.find('br').get(0).next.data feels hacky?
+function getProfileBlurb($profile) {
+  var blurbNode = $profile.find('br').get(0);
+
+  if(blurbNode && blurbNode.next) {
+    return blurbNode.next.data;
+  } else {
+    return 'No blurb found. You should bother this person about their lack of blurb';
+  }
+}
+
 module.exports = [
   {
     name: 'Jeff Lee',
@@ -7,3 +53,17 @@ module.exports = [
     imgSrc: 'http://zignallabs.com/wp-content/uploads/2015/10/Jeff_Lee.png'
   }
 ];
+
+function convertObjToArray(obj) {
+  var arr = [];
+
+  for(var i = 0; i < obj.length; i++) {
+    arr.push(obj[i]);
+  }
+
+  return arr;
+}
+
+mineZignalTeamPage().then(function(result) {
+  console.log(result);
+});
